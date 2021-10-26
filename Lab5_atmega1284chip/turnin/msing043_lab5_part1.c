a3d90bd8cd86afba85597182631839721c722ba3
msing043 <58718116+msing043@users.noreply.github.com>
2021-10-25 20:53:49 -0700

    Update msing043_lab5_part1.c

--- Lab5_atmega1284chip/turnin/msing043_lab5_part1.c
+++ Lab5_atmega1284chip/turnin/msing043_lab5_part1.c
@@ -1,7 +1,7 @@
 /*	Author: msing043
  *  Partner(s) Name: 
- *	Lab Section:023
- *	Assignment: Lab #5  Exercise #1
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,38 +12,37 @@
 #include "simAVRHeader.h"
 #endif
 
+
 int main(void) {
 
     DDRA = 0x00; PORTA = 0xFF;
     DDRC = 0xFF; PORTC = 0x00;
-    unsigned char tmpA = 0x00;
-    unsigned char tmpB = 0x00;
+    unsigned char tempA = 0x00;
+    unsigned char tempB = 0x00;
 
+	
 while(1){
-tmpA=~PINA;
-tmpB=0x00;
-if (((tmpA & 0x0F) == 0x0F) || ((tmpA & 0x0E) == 0x0E) || ((tmpA & 0x0D) == 0x0D)) {
-                tmpB = 0x3F;
+tempA=~PINA;
+tempB=0x00;
+if (((tempA&0x0F)==0x0F)||((tempA&0x0E)==0x0E)||((tempA&0x0D)==0x0D)) {
+        tempB = 0x3F;
 }
-else if (((tmpA & 0x0C) == 0x0C) || ((tmpA & 0x0B) == 0x0B) || ((tmpA & 0x0A) == 0x0A)) {
-                tmpB = 0x3E;
+else if (((tempA&0x0C)==0x0C)||((tempA&0x0B)==0x0B)||((tempA&0x0A)==0x0A)) {
+	tempB = 0x3E;
 }
-else if (((tmpA & 0x09) == 0x09) || ((tmpA & 0x08) == 0x08) || ((tmpA & 0x07) == 0x07)) {
-                tmpB = 0x3C;
+else if (((tempA&0x09)==0x09)||((tempA&0x08)==0x08)||((tempA&0x07)==0x07)) {
+        tempB = 0x3C;
 }
-else if (((tmpA & 0x06) == 0x06) || ((tmpA & 0x06) == 0x06)) {
-                tmpB = 0x38;
+else if (((tempA&0x06)==0x06)||((tempA &0x05)==0x05)) {
+	tempB=0x38;
 }
-else if (((tmpA & 0x04) == 0x04) || ((tmpA & 0x03) == 0x03)) {
-                tmpB = 0x70;
-}
-else if (((tmpA & 0x02) == 0x02) || ((tmpA & 0x01) == 0x01)) {
-		tmpB = 0x60;
+else if (((tempA&0x04)==0x04)||((tempA&0x03)==0x03)) {
+        tempB = 0x70;
 }
 else {
-	tmpB = 0x40;
+	tempB = 0x40; //empty gas tank?
 }
-PORTC=tmpB;
+PORTC=tempB;
 }
 return 1;
 }
